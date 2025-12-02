0d1e8355a5e62d718d19724671599190edaf5094
Nehathukrul-3128 <nehathukrul31@gmail.com>
2025-12-02 09:44:15 +0000

    final

--- userproject/src/components/Register.jsx
+++ userproject/src/components/Register.jsx
@@ -57,12 +57,12 @@ export default function Register()
             <br /><br />
 
              <label>Enter Password</label>
-            <input type="text" name="upass" onChange={handleChange} />
+            <input type="password" name="upass" onChange={handleChange} />
              {errors.upass && <p><mark>{errors.upass}</mark></p>}
             <br /><br />
 
              <label>Enter Confirm Pssword</label>
-            <input type="text" name="ucpass" onChange={handleChange} />
+            <input type="password" name="ucpass" onChange={handleChange} />
              {errors.ucpass && <p><mark>{errors.ucpass}</mark></p>}
             <br /><br />
             <input type="submit" value={"Submit"} />
